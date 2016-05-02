60091a25e80ff71678496a6451f3c1dba5505741
jivillag <constanzavillagran@hotmail.com>
2016-05-02 20:22:44 -0300

    a

--- frontend/views/facultad/index.php
+++ frontend/views/facultad/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Crear Facultad', ['crear'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Crear Facultad', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
