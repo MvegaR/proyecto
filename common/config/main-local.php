f1e0762a986550d91487fd5567be0d550ff4222c
MvegaR <mvegar@alumnos.ubiobio.cl>
2016-05-07 14:45:58 -0300

    ReCaptcha configuración bien xD

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -16,5 +16,10 @@ return [
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
+        'reCaptcha' => [
+        'name' => 'reCaptcha',
+        'class' => 'himiklab\yii2\recaptcha\ReCaptcha',
+        'siteKey' => '6LfD6hITAAAAAGcfqyVwxtpOnvUHXsYWrv0pSgnJ',
+        'secret' => '6LfD6hITAAAAAEdV6MQ8zDX3emwQY4bVYyw-L3nz',]
     ],
 ];
--- common/config/main.php
+++ common/config/main.php
@@ -5,15 +5,9 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        
     ],
     'language' => 'es_ES',
-    'components' => [
-    'reCaptcha' => [
-        'name' => 'reCaptcha',
-        'class' => 'himiklab\yii2\recaptcha\ReCaptcha',
-        'siteKey' => '6LfD6hITAAAAAGcfqyVwxtpOnvUHXsYWrv0pSgnJ',
-        'secret' => '6LfD6hITAAAAAEdV6MQ8zDX3emwQY4bVYyw-L3nz',
-    ],
 ];
 
 
