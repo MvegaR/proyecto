884ff33dd7c304c1ee0d3f15bcd4f2fa390dfca7
jivillag <constanzavillagran@hotmail.com>
2016-05-09 22:07:28 -0300

    docente

--- frontend/views/docente/_form.php
+++ frontend/views/docente/_form.php
@@ -15,7 +15,7 @@ use frontend\models\Facultad;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'ID_DOCENTE')->textInput(['maxlength' => true])->label("RUT Docente") ?>
+    <?= $form->field($model, 'ID_DOCENTE')->textInput(['maxlength' => true])->label("RUT Docente")->input('rut', ['placeholder' => "Ingrese rut con puntos y guion"]) ?>
 
     <?= $form->field($model, 'ID_ROL')->dropDownList(
         ArrayHelper::map(Rol::find()->all(),'ID_ROL','NOMBRE_ROL'),
@@ -25,13 +25,13 @@ use frontend\models\Facultad;
         ArrayHelper::map(Facultad::find()->all(),'ID_FACULTAD','NOMBRE_FACULTAD'),
         ['prompt'=>'Seleccione Facultad'] )->label('Facultad') ?>
 
-    <?= $form->field($model, 'NOMBRE_DOCENTE')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'NOMBRE_DOCENTE')->textInput(['maxlength' => true])->input('nombre', ['placeholder' => "Ingrese nombre"]) ?>
 
-    <?= $form->field($model, 'EMAIL')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'EMAIL')->textInput(['maxlength' => true])->input('email', ['placeholder' => "Ingrese email"]) ?>
 
-    <?= $form->field($model, 'USER')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'USER')->textInput(['maxlength' => true])->input('user', ['placeholder' => "Ingrese nombre de usuario"]) ?>
 
-    <?= $form->field($model, 'PASSWORD')->passwordInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'PASSWORD')->passwordInput(['maxlength' => true])->input('password', ['placeholder' => "Ingrese clave de usuario"]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
