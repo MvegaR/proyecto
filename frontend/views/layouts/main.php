1ca770e9b1115f736493141e85d95b93f5551f7b
MvegaR <mvegar@alumnos.ubiobio.cl>
2016-05-16 02:22:42 -0400

    Ocultando items del menu por ROL, faltan los link xDD

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -114,7 +114,7 @@ AppAsset::register($this);
 <main class="panel-primary" id="panelPrimario" 
 style="background-color:#0064AC; background-image:url(img/bg.png);color:#fff;  position:relative; top:-4px; height:200px">
   <div class="container">
-  <span class= 'pull-right col-xs-0 col-md-2 col-lg-2' style="position:relative; margin:0px;"> <img src="img/Working-Schedule.png" alt="Placeholder image" width="200" height="200" class="img-responsive visible-md visible-lg"></span> 
+  <span class= 'pull-right hidden-xs col-md-2 col-lg-2' style="position:relative; margin:0px;"> <img src="img/Working-Schedule.png" alt="Placeholder image" width="200" height="200" class="img-responsive visible-md visible-lg"></span> 
    <h1><strong>Proyecto DSI</strong> </h1>
       <h3 class="col-xs-12 col-md-10 col-lg-10" style=" text-align:justify;">Sistema de información dedicado a la asignación de horarios y salas de clases para la Universidad del Bío-Bío.</h3>  
 
@@ -131,15 +131,15 @@ style="background-color:#0064AC; background-image:url(img/bg.png);color:#fff;  p
     <div class="list-group">
         <div class=" text-center" style="background-color:#0064AC; color:#FFFFFF; border-radius: 30px 30px 0px 0px; font-weight: bold; font-size:19px;">Link rápidos</div>
 
-      <a href="#" class="list-group-item pulso">Módulo de administración</a>
-      <a href="#" class="list-group-item pulso">Ver Horario</a>
-      <a href="#" class="list-group-item pulso">Formulario de asignación</a>
-      <a href="#" class="list-group-item pulso">Solicitud cancelación</a>
-      <a href="#" class="list-group-item pulso">Solicitud cambio de sala</a>
+      <?php if(!Yii::$app -> user -> isGuest && Yii::$app -> user -> identity -> ID_ROL == 1){?><a href="#" class="list-group-item pulso">Módulo de administración</a><?php } ?>
+      <?php if(!Yii::$app -> user -> isGuest && Yii::$app -> user -> identity -> ID_ROL == 2){?><a href="#" class="list-group-item pulso">Ver Horario</a><?php } ?>
+      <?php if(!Yii::$app -> user -> isGuest && Yii::$app -> user -> identity -> ID_ROL == 2){?><a href="#" class="list-group-item pulso">Formulario de asignación</a><?php } ?>
+      <?php if(!Yii::$app -> user -> isGuest && Yii::$app -> user -> identity -> ID_ROL == 2){?><a href="#" class="list-group-item pulso">Solicitud cancelación</a><?php } ?>
+      <?php if(!Yii::$app -> user -> isGuest && Yii::$app -> user -> identity -> ID_ROL == 2){?><a href="#" class="list-group-item pulso">Solicitud cambio de sala</a><?php } ?>
       <a href="#" class="list-group-item pulso">Formulario denuncia mal uso</a>
       <a href="#" class="list-group-item pulso">Listado de salas</a>
       <a href="#" class="list-group-item pulso">Estadísticas</a>
-      <a href="#" class="list-group-item pulso">Cambiar usuario o contraseña</a>
+      <?php if(!Yii::$app -> user -> isGuest){?><a href="#" class="list-group-item pulso">Cambiar usuario o contraseña</a><?php } ?>
     </div>
   </div>
 
