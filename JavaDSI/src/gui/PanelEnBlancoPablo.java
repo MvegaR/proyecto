309dd8451e27ff9866724c8971c416389b71fb2c
pabloooh18 <pansoto@alumnos.ubiobio.cl>
2016-05-23 23:52:55 -0300

    Ventana rapida de user y password

--- JavaDSI/src/gui/PanelEnBlancoPablo.java
+++ JavaDSI/src/gui/PanelEnBlancoPablo.java
@@ -1,15 +1,54 @@
 package gui;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 public class PanelEnBlancoPablo extends JPanel {
+	private JTextField texto_user;
+	private JPasswordField password_texto;
 
     /**
      * Create the panel.
      * PANEL DE PABLO
      */
     public PanelEnBlancoPablo() {
+    	setLayout(null);
+    	
+    	JLabel label_user = new JLabel("User");
+    	label_user.setBounds(106, 86, 76, 20);
+    	add(label_user);
+    	
+    	JLabel label_password = new JLabel("Password");
+    	label_password.setBounds(106, 139, 76, 20);
+    	add(label_password);
+    	
+    	texto_user = new JTextField();
+    	texto_user.setBounds(192, 86, 129, 20);
+    	add(texto_user);
+    	texto_user.setColumns(10);
+    	
+    	password_texto = new JPasswordField();
+    	password_texto.setBounds(192, 139, 129, 20);
+    	add(password_texto);
+    	
+    	JButton btnEntrar = new JButton("Entrar");
+    	btnEntrar.setBounds(93, 216, 89, 23);
+    	add(btnEntrar);
+    	
+    	JButton btnSalir = new JButton("Salir");
+    	btnSalir.addActionListener(new ActionListener() {
+    		public void actionPerformed(ActionEvent arg0) {
+    			System.exit(0);
+    		}
+    	});
+    	btnSalir.setBounds(269, 216, 89, 23);
+    	add(btnSalir);
 
     }
-
 }
