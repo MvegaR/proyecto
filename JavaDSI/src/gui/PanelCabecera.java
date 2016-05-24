322ae2231cce2c935f5400d15c5b130c6192f91a
Raul Opazo <raopazo@alumnos.ubiobio.cl>
2016-05-24 09:19:59 -0400

    Panel cabecera y ventana principal

---
+++ JavaDSI/src/gui/PanelCabecera.java
@@ -0,0 +1,40 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Font;
+
+public class PanelCabecera extends JPanel {
+
+	private Image imagen;
+	
+	public PanelCabecera() {
+		this.setBackground(Color.BLUE);      
+		String texto = "<html><body><h1><strong>Proyecto DSI</strong></h1>Sistema de información dedicado a la asignación de horarios y salas de clases para la universidad del Bío-Bío.</body></html>";
+		JLabel lblNewLabel = new JLabel(texto);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setToolTipText("");
+		add(lblNewLabel);
+	    imagen = new ImageIcon("recursos/texturaOP.png").getImage();
+	}
+	
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int i = imagen.getWidth(this);
+        int j = imagen.getHeight(this);
+        if (i > 0 && j > 0) {
+            for (int x = 0; x < getWidth(); x += i) {
+                for (int y = 0; y < getHeight(); y += j) {
+                    g.drawImage(imagen, x, y, i, j, this);
+                }
+            }
+        }
+    }
+
+}
---
+++ JavaDSI/src/gui/VentanaPrincipal.java
@@ -0,0 +1,34 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import de.javasoft.plaf.synthetica.SyntheticaBlackEyeLookAndFeel;
+import java.awt.BorderLayout;
+
+public class VentanaPrincipal extends JFrame {
+
+	public static void main(String[] args) throws Exception {
+		
+		UIManager.setLookAndFeel(new SyntheticaBlackEyeLookAndFeel()); // cambia el estilo
+		VentanaPrincipal vp = new VentanaPrincipal();
+		vp.setVisible(true);
+
+	}
+	
+    private PanelEnBlancoRaul panelRaul;
+    
+	private PanelCabecera cabecera;
+	
+	public VentanaPrincipal() {
+		panelRaul = new PanelEnBlancoRaul();
+		cabecera = new PanelCabecera();
+		
+		this.add(cabecera,BorderLayout.NORTH);
+		this.add(panelRaul,BorderLayout.CENTER);
+        this.setSize(1024, 720);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+	}
+
+}
