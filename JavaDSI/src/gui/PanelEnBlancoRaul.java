f4a9a13e1e2f5f2ebb713ec16c0c51462111b6b8
Raul Opazo <raopazo@alumnos.ubiobio.cl>
2016-05-24 09:15:02 -0400

    Panel que contiene paneles XD

--- JavaDSI/src/gui/PanelEnBlancoRaul.java
+++ JavaDSI/src/gui/PanelEnBlancoRaul.java
@@ -1,15 +1,26 @@
 package gui;
 
 import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.BorderLayout;
 
 public class PanelEnBlancoRaul extends JPanel {
 
-    /**
-     * Create the panel.
-     * PANEL DE RAÚL
-     */
+    private PanelEnBlancoConi coni;
+    private PanelEnBlancoMarcos marcos;
+    private PanelEnBlancoMari mari;
+    private PanelEnBlancoPablo pablo;
+	
     public PanelEnBlancoRaul() {
-
+    	coni = new PanelEnBlancoConi();
+    	marcos = new PanelEnBlancoMarcos();
+    	mari = new PanelEnBlancoMari();
+    	pablo = new PanelEnBlancoPablo();
+    	add(marcos);
+    	add(coni);
+    	add(mari);
+    	add(pablo);
     }
 
 }
