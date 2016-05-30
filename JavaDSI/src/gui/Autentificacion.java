25c4949a113c10dc4e3b1e6475a266f9b15f32c3
MvegaR <mvegar@alumnos.ubiobio.cl>
2016-05-30 19:02:47 -0400

    Cambios de nombre PanelEnBlancoPablo -> Auntenficación

--- JavaDSI/src/gui/Autentificacion.java
+++ JavaDSI/src/gui/Autentificacion.java
@@ -7,58 +7,63 @@ import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Font;
 
 
 public class Autentificacion extends JPanel {
-    /**
+	/**
      * 
      */
     private static final long serialVersionUID = 1L;
-    private JTextField texto_user;
-    private JPasswordField password_texto;
-    private JButton btnEntrar;
+	private JTextField texto_user;
+	private JPasswordField password_texto;
+	private JButton btnEntrar;
 
     /**
      * Create the panel.
      * PANEL DE PABLO
      */
     public Autentificacion() {
-	setLayout(null);
-
-	JLabel label_user = new JLabel("User");
-	label_user.setBounds(106, 86, 76, 20);
-	add(label_user);
-
-	JLabel label_password = new JLabel("Password");
-	label_password.setBounds(106, 139, 76, 20);
-	add(label_password);
-
-	texto_user = new JTextField();
-	texto_user.setBounds(192, 86, 129, 20);
-	add(texto_user);
-	texto_user.setColumns(10);
-
-	password_texto = new JPasswordField();
-	password_texto.setBounds(192, 139, 129, 20);
-	add(password_texto);
-
-	btnEntrar = new JButton("Entrar");
-	btnEntrar.setBounds(93, 216, 89, 23);
-	add(btnEntrar);
-
-	JButton btnSalir = new JButton("Salir");
-	btnSalir.addActionListener(new ActionListener() {
-	    public void actionPerformed(ActionEvent arg0) {
-		System.exit(0);
-	    }
-	});
-	btnSalir.setBounds(269, 216, 89, 23);
-	add(btnSalir);
+    	
+    	JLabel label_user = new JLabel("User");
+    	label_user.setFont(new Font("Tahoma", Font.PLAIN, 17));
+    	label_user.setBounds(67, 86, 115, 32);
+    	
+    	JLabel label_password = new JLabel("Password");
+    	label_password.setFont(new Font("Tahoma", Font.PLAIN, 17));
+    	label_password.setBounds(67, 139, 115, 32);
+    	
+    	texto_user = new JTextField();
+    	texto_user.setBounds(192, 86, 206, 32);
+    	texto_user.setColumns(10);
+    	
+    	password_texto = new JPasswordField();
+    	password_texto.setBounds(192, 139, 206, 32);
+    	
+    	btnEntrar = new JButton("Entrar");
+    	btnEntrar.setBounds(93, 216, 134, 43);
+    	
+    	JButton btnSalir = new JButton("Salir");
+    	btnSalir.setBounds(269, 216, 129, 43);
+    	btnSalir.addActionListener(new ActionListener() {
+    		public void actionPerformed(ActionEvent arg0) {
+    			System.exit(0);
+    		}
+    	});
+    	setLayout(null);
+    	add(label_user);
+    	add(texto_user);
+    	add(label_password);
+    	add(password_texto);
+    	add(btnEntrar);
+    	add(btnSalir);
 
     }
-
+    
     public JButton getBtnEntrar(){
-	return btnEntrar;
+    	return btnEntrar;
     }
-
+    
 }
