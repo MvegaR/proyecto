404bfed0525f97761605a552ade115560db87b36
pabloooh18 <pansoto@alumnos.ubiobio.cl>
2016-06-05 15:24:26 -0400

    Horario Docente

---
+++ frontend/controllers/HorarioDocenteController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use frontend\models\Seccion;
+use frontend\models\Asignatura;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+
+class HorarioDocenteController extends Controller
+{
+    
+    
+    public function actionIndex(){
+
+        $array = array();
+        $id = Yii::$app -> user -> identity -> ID_DOCENTE;
+        for($i = 1, $j = 1; $i < 7; $i++, $j++){
+            $var1 = Seccion::getHORARIOSPORPROFESOR($i,'08:10:00',$id);
+            $var2 = Seccion::getHORARIOSPORPROFESOR($i,'08:50:00',$id);
+            $var3 = Seccion::getHORARIOSPORPROFESOR($i,'09:40:00',$id);
+            $var4 = Seccion::getHORARIOSPORPROFESOR($i,'10:20:00',$id);
+            $var5 = Seccion::getHORARIOSPORPROFESOR($i,'11:10:00',$id);
+            $var6 = Seccion::getHORARIOSPORPROFESOR($i,'11:50:00',$id);
+            $var7 = Seccion::getHORARIOSPORPROFESOR($i,'12:40:00',$id);
+            $var8 = Seccion::getHORARIOSPORPROFESOR($i,'13:20:00',$id);
+            $var9 = Seccion::getHORARIOSPORPROFESOR($i,'14:10:00',$id);
+            $var10 = Seccion::getHORARIOSPORPROFESOR($i,'14:50:00',$id);
+            $var11 = Seccion::getHORARIOSPORPROFESOR($i,'15:40:00',$id);
+            $var12 = Seccion::getHORARIOSPORPROFESOR($i,'16:20:00',$id);
+            $var13 = Seccion::getHORARIOSPORPROFESOR($i,'17:10:00',$id);
+            $var14 = Seccion::getHORARIOSPORPROFESOR($i,'17:50:00',$id);
+            $var15 = Seccion::getHORARIOSPORPROFESOR($i,'18:40:00',$id);
+            $var16 = Seccion::getHORARIOSPORPROFESOR($i,'19:20:00',$id);
+            $var17 = Seccion::getHORARIOSPORPROFESOR($i,'20:10:00',$id);
+            $var18 = Seccion::getHORARIOSPORPROFESOR($i,'20:50:00',$id);
+            $var19 = Seccion::getHORARIOSPORPROFESOR($i,'21:40:00',$id);
+            $var20 = Seccion::getHORARIOSPORPROFESOR($i,'22:20:00',$id);
+            array_push($array,$var1,$var2,$var3,$var4,$var5,
+                             $var6,$var7,$var8,$var9,$var10,
+                             $var11,$var12,$var13,$var14,$var15,
+                             $var16,$var17,$var18,$var19,$var20
+                    );
+        }
+       
+        return $this->render('index', [
+            'array' => $array,
+        ]);
+    }
+
+}
---
+++ frontend/views/horario-docente/index.php
@@ -0,0 +1,67 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\BloqueSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Horario Profesor';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="Seccion-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+   <div class= "table-responsive">
+    <table class="table table-bordered">
+    <tr>
+        <th>Hora</th>
+        <th>Lunes</th>
+        <th>Martes</th>
+        <th>Miercoles</th>
+        <th>Jueves</th>
+        <th>Viernes</th>
+        <th>Sabado</th>
+    </tr>
+    <?php 
+        for($j = 0; $j < 20; $j++){
+            if($j == 0) echo '<tr><th>08:10 - 08:50</th>';
+            else if($j == 1) echo '<tr><th>08:50 - 09:30</th>';
+            else if($j == 2) echo '<tr><th>09:40 - 10:20</th>';
+            else if($j == 3) echo '<tr><th>10:20 - 11:00</th>';
+            else if($j == 4) echo '<tr><th>11:10 - 11:50</th>';
+            else if($j == 5) echo '<tr><th>11:50 - 12:30</th>';
+            else if($j == 6) echo '<tr><th>12:40 - 13:20</th>';
+            else if($j == 7) echo '<tr><th>13:20 - 14:00</th>';
+            else if($j == 8) echo '<tr><th>14:10 - 14:50</th>';
+            else if($j == 9) echo '<tr><th>14:50 - 15:30</th>';
+            else if($j == 10) echo '<tr><th>15:40 - 16:20</th>';
+            else if($j == 11) echo '<tr><th>16:20 - 17:00</th>';
+            else if($j == 12) echo '<tr><th>17:10 - 17:50</th>';
+            else if($j == 13) echo '<tr><th>17:50 - 18:30</th>';
+            else if($j == 14) echo '<tr><th>18:40 - 19:20</th>';
+            else if($j == 15) echo '<tr><th>19:20 - 20:00</th>';
+            else if($j == 16) echo '<tr><th>20:10 - 20:50</th>';
+            else if($j == 17) echo '<tr><th>20:50 - 21:30</th>';
+            else if($j == 18) echo '<tr><th>21:40 - 22:20</th>';
+            else if($j == 19) echo '<tr><th>22:20 - 23:00</th>';
+            for($i = 0; $i < 6; $i++){
+                       
+                         if($array[$j+(20*$i)] != null){
+                            $row=$array[$j+(20*$i)];
+                            echo '<td class="success">',$row[0]['nombre_asignatura'],'</td>';
+                           }else{
+                            $row = [['nombre_asignatura'=>NULL]];
+                            echo '<td>',$row[0]['nombre_asignatura'],'</td>';
+                           }
+                         
+            }
+            
+            echo '</tr>';
+        }
+    ?>
+    </table>
+</div>
+</div>
