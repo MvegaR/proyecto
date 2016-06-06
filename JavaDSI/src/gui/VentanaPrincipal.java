c0f163c5851949aee2490008267a54301def3d4a
MvegaR <mvegar@alumnos.ubiobio.cl>
2016-06-06 12:48:05 -0400

    Las listas, ahora falta una funcion para rellenarlas con la base de datos....

--- JavaDSI/src/gui/VentanaPrincipal.java
+++ JavaDSI/src/gui/VentanaPrincipal.java
@@ -5,8 +5,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
+import db.Asignatura;
+import db.Bloque;
+import db.Carrera;
+import db.Departamento;
+import db.Docente;
+import db.Edificio;
+import db.Facultad;
+import db.Sala;
+import db.Seccion;
 import de.javasoft.plaf.synthetica.SyntheticaBlueSteelLookAndFeel;
 import java.awt.BorderLayout;
+import java.util.ArrayList;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -24,6 +34,103 @@ public class VentanaPrincipal extends JFrame {
     private PanelCabecera cabecera;
     private JPanel seccionOpciones;
     public static JButton btnVolver;
+    
+    //---------------------------------------------------------------------
+    //Se debe rellenar estas listas cada vez que se inicie le programa:
+    //ArrayList por que no vamos a insertar más... pero vamos a buscar mucho.
+    
+     private ArrayList<Asignatura> asignaturas = new ArrayList<>();
+     private ArrayList<Bloque> bloques = new ArrayList<>();
+     private ArrayList<Carrera> carreras = new ArrayList<>();
+     private ArrayList<Departamento> departamentos = new ArrayList<>();
+     private ArrayList<Docente> docentes = new ArrayList<>();
+     private ArrayList<Edificio> edificios = new ArrayList<>();
+     private ArrayList<Facultad> facultades = new ArrayList<>();
+     private ArrayList<Sala> salas = new ArrayList<>();
+     private ArrayList<Seccion> secciones = new ArrayList<>();
+    //---------------------------------------------------------------------
+    
+
+    /**
+     * @return the asignaturas
+     */
+    public ArrayList<Asignatura> getAsignaturas() {
+        return asignaturas;
+    }
+
+
+
+    /**
+     * @return the bloques
+     */
+    public ArrayList<Bloque> getBloques() {
+        return bloques;
+    }
+
+
+
+    /**
+     * @return the carreras
+     */
+    public ArrayList<Carrera> getCarreras() {
+        return carreras;
+    }
+
+
+
+    /**
+     * @return the departamentos
+     */
+    public ArrayList<Departamento> getDepartamentos() {
+        return departamentos;
+    }
+
+
+
+    /**
+     * @return the docentes
+     */
+    public ArrayList<Docente> getDocentes() {
+        return docentes;
+    }
+
+
+
+    /**
+     * @return the edificios
+     */
+    public ArrayList<Edificio> getEdificios() {
+        return edificios;
+    }
+
+
+
+    /**
+     * @return the facultades
+     */
+    public ArrayList<Facultad> getFacultades() {
+        return facultades;
+    }
+
+
+
+    /**
+     * @return the salas
+     */
+    public ArrayList<Sala> getSalas() {
+        return salas;
+    }
+
+
+
+    /**
+     * @return the secciones
+     */
+    public ArrayList<Seccion> getSecciones() {
+        return secciones;
+    }
+
+
 
     public VentanaPrincipal() {
 	this.setResizable(false);
