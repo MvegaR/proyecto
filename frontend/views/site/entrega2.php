1f364583bb1f10e4481056544f5bb553098c08e6
MvegaR <mvegar@alumnos.ubiobio.cl>
2016-06-11 01:02:25 -0400

    Cosas faltantes

--- frontend/views/site/entrega2.php
+++ frontend/views/site/entrega2.php
@@ -4,32 +4,38 @@
 
 use yii\helpers\Html;
 
-$this->title = 'Segunda semana de desarrollo';
+$this->title = 'Cosas faltantes';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
-    Falta: <br>
-    <br>
-    Funcionalidad: <br>
-    -Ver horario usuario autentificado (docente)<br>
-    -Importar desde excel.<br>
-
+    Funcionalia:<br>
+    -Importación de la BD completa desde un solo excel. 
     <br>
     Estetica:<br>
     -Cambiar numeros ID por su Nombre, en todos los index.php y view.php siguiento el video (ejemplo ya hecho index.php de docente) -> https://www.youtube.com/watch?v=OLfz7Iy_y84<br>
     -Migas de pan (breadcrumbs) y que diferencie admin de docente y invitado<br>
+    En<br><br>
+    edificio -> id facultad<br>
+bloque -> id Dia<br>
+Asignatura -> Id carrera<br>
+Solicitudes asignación de sala ->  estado, docente, tipo sala.<br>
+Solicitudes de Asignacion Temporal -> estado, docente, tipo sala.<br>
+Solicitud de cambio -> estado, docente.<br>
+Solicitudes cancelacion -> estado, docente, <br>
+reportes de sala (post de denuncia) -> tipo denuncia, estado denuncia, facultad, edificio.<br>
 
-    <p><?= Html::a('RFW02 Importación de datos (Marcos) (PENDIENTE)', [''], ['class'=>'']) ?></p>
+
+
+
+    <p><?= Html::a('RFW02 Importación de datos (Marcos) (PENDIENTE 80%)', [''], ['class'=>'']) ?></p>
     <p><?= Html::a('RFW09 Generación de usuario y contraseña docentes (Pablo)', ['docente/create'], ['class'=>'']) ?></p>
     <p><?= Html::a('RFW10 Administración de asignaturas (Raul)', ['asignatura/index'], ['class'=>'']) ?></p>
     <p><?= Html::a('RFW12 CTRL+Z (Raul)', ['solicitud-cancelacion/create'], ['class'=>'']) ?></p>
     <p><?= Html::a('RFW13 Cambio de sala (Raul)', ['solicitud-cambio/create'], ['class'=>'']) ?></p>
     <p><?= Html::a('RFW14 Anulación de sala (María)', ['solicitud-cambio/create'], ['class'=>'']) ?></p>
         
-    <p><?= Html::a('RFW16 Ver horario (pablo) (PENDIENTE)', [''], ['class'=>'']) ?></p>
-        -Es el horario desde un docente (usuario conectado)<br><br>
+    <p><?= Html::a('RFW16 Ver horario (pablo)', [''], ['class'=>'']) ?></p>
         
     <p><?= Html::a('RFW17 Información salas (Marí)', ['sala/lista'], ['class'=>'']) ?></p>
         
