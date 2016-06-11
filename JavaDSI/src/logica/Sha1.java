ba830b4e8dd2163eeb94b1c55a26958daa44aab8
MvegaR <mvegar@alumnos.ubiobio.cl>
2016-06-11 17:06:27 -0400

    fix

--- JavaDSI/src/logica/Sha1.java
+++ JavaDSI/src/logica/Sha1.java
@@ -17,17 +17,17 @@ public class Sha1 {
 	        MessageDigest mDigest;
 		try {
 		    mDigest = MessageDigest.getInstance("SHA1");
+		    byte[] result = mDigest.digest(input.getBytes());
+		    StringBuffer sb = new StringBuffer();
+		        for (int i = 0; i < result.length; i++) {
+		            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+		        }
+		        return sb.toString();
 		} catch (NoSuchAlgorithmException e) {
 		    // TODO Auto-generated catch block
 		    e.printStackTrace();
 		}
-	        byte[] result = mDigest.digest(input.getBytes());
-	        StringBuffer sb = new StringBuffer();
-	        for (int i = 0; i < result.length; i++) {
-	            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-	        }
-	         
-	        return sb.toString();
+		 return null;
 	    }
 	}
 
