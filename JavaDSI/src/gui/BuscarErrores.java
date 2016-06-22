7f1061dffbf71802bac657e96f5867fd3514816f
jivillag <constanzavillagran@hotmail.com>
2016-06-22 19:44:02 -0400

    Clase

--- JavaDSI/src/gui/BuscarErrores.java
+++ JavaDSI/src/gui/BuscarErrores.java
@@ -38,6 +38,46 @@ public class BuscarErrores {
     }
     
     
+    public void SinAsignaturas(){
+    	 if(asignaturas.isEmpty())
+    	  MensajesError.MeEr_SinAsignaturas();
+    	}
+    
+    public void SinBloques(){
+   	 if(asignaturas.isEmpty())
+   	  MensajesError.MeEr_SinBloques();
+   	}
+    
+    public void SinCarreras(){
+      	 if(asignaturas.isEmpty())
+      	  MensajesError.MeEr_SinCarreras();
+      	}
+    
+    public void SinDepartamento(){
+     	 if(asignaturas.isEmpty())
+     	  MensajesError.MeEr_SinDepartamentos();
+     	}
+    
+    public void SinDocentes(){
+     	 if(asignaturas.isEmpty())
+     	  MensajesError.MeEr_SinDocentes();
+     	}
+    
+    public void SinEdificios(){
+     	 if(asignaturas.isEmpty())
+     	  MensajesError.MeEr_SinEdificios();
+     	}
+    
+    public void SinSalas(){
+     	 if(asignaturas.isEmpty())
+     	  MensajesError.MeEr_SinSalas();
+     	}
+    
+    public void SinSecciones(){
+     	 if(asignaturas.isEmpty())
+     	  MensajesError.MeEr_SinSecciones();
+     	}
+    
     public void AsignaturasSinCarreras(){
         for (int i = 0; i < asignaturas.size(); i++) {
             if(asignaturas.get(i).getIdCarrera() == null){
@@ -54,5 +94,76 @@ public class BuscarErrores {
     	}
     }
     
+    public void SeccionSinDocente(){
+    	for(int i =0; i< secciones.size(); i++){
+    		if(secciones.get(i).getIdDocente() == null){
+    			MensajesError.meEr_SeccionSinDocentes();
+    		}
+    	}
+    }
+    
+    public void SalasSinEdificio(){
+    	for(int i=0; i< salas.size(); i++){
+    		if(salas.get(i).getIdEdificio()== null){
+    			MensajesError.meEr_SalasSinEdificios();
+    		}
+    	}
+    }
+    
+    public void BloquesSinSalas(){
+    	for(int i=0; i<bloques.size(); i++){
+    		if(bloques.get(i).getIdSala() == null){
+    			MensajesError.meEr_BloquesSinSalas();
+    		}
+    	}
+    }
+    
+    public void BloquesSinSecciones(){
+    	for(int i=0; i<bloques.size(); i++){
+    		if(bloques.get(i).getIdSeccion() == null){
+    			MensajesError.meEr_BloquesSinSecciones();
+    		}
+    	}
+    }
+    
+    public void DepartamentoSinFacultad(){
+    	for(int i=0; i< departamentos.size(); i++){
+    		if(departamentos.get(i).getIdFacultad() == null){
+    			MensajesError.meEr_DepartamentoSinFacultad();
+    		}
+    	}
+    }
+    
+    public void DocenteSinDepartamento(){
+    	for(int i=0; i< docentes.size(); i++){
+    		if(docentes.get(i).getIdDepartamento() == null){
+    			MensajesError.meEr_DocenteSinDepartamento();
+    		}
+    	}
+    }
+    
+    public void EdificioSinFacultad(){
+    	for(int i=0; i< edificios.size(); i++){
+    		if(edificios.get(i).getIdFacultad() == null){
+    			MensajesError.meEr_EdificioSinFacultad();
+    		}
+    	}
+    }
+    
+    public void FacultadSinDepartamento(){
+    	for(int i=0; i<facultades.size(); i++){
+    		if(facultades.get(i).getNombreDepartamento() == null){
+    			MensajesError.meEr_FacultadSinDepartamento();
+    		}
+    	}
+    }
+    
+    public void SeccionSinAsignatura(){
+    	for(int i =0; i< secciones.size(); i++){
+    		if(secciones.get(i).getIdAsignatura() == null){
+    			MensajesError.meEr_SeccionSinAsignaturas();
+    		}
+    	}
+    }
     
 }
