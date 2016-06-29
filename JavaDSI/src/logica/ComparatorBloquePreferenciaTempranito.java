51a9daaf367cfe76a3325c4c5c2d73c47c4c92ee
MvegaR <mvegar@alumnos.ubiobio.cl>
2016-06-29 00:14:43 -0400

    +Comentarios

--- JavaDSI/src/logica/ComparatorBloquePreferenciaTempranito.java
+++ JavaDSI/src/logica/ComparatorBloquePreferenciaTempranito.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import db.Bloque;
 
 public class ComparatorBloquePreferenciaTempranito implements Comparator<Bloque>{
-  //3. Se prefiere temprano a tarde ...
+  //3. Se prefiere temprano a tarde ... (Listo)
    public int compare(Bloque o1, Bloque o2) {
        int r = o1.getInicio().compareTo(o2.getInicio());
        if(r == 0){
--- JavaDSI/src/logica/ComparatorDeSalaParaPreferencias.java
+++ JavaDSI/src/logica/ComparatorDeSalaParaPreferencias.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import db.Sala;
 
 public class ComparatorDeSalaParaPreferencias implements Comparator<Sala>{
-  //2. Se prefiere la sala con capacidad con diferencia a cupo igual o mayor a cero y mientras menor mejor.
+  //2. Se prefiere la sala con capacidad con diferencia a cupo igual o mayor a cero y mientras menor mejor. (Listo)
  
     
     @Override
