46c5a26f4526b79c814b565e1c1bdd26b900c0b3
MvegaR <mvegar@alumnos.ubiobio.cl>
2016-07-01 02:59:35 -0400

    php 5 ¬¬

--- frontend/controllers/SeccionController.php
+++ frontend/controllers/SeccionController.php
@@ -154,6 +154,6 @@ class SeccionController extends Controller
     }
     //entrega la capacidad de una seccion
     public function actionLists2($id){
-        echo (Seccion::find()->where(["ID_SECCION"=>$id])->one())-> CUPO;
+        echo Seccion::find()->where(["ID_SECCION"=>$id])->one()-> CUPO;
     }
 }
